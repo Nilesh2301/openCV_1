e072f32bea8a8b67089635433799787691b34886
Nilesh Gupta <nileshkrishesh@gmail.com>
2025-11-02 11:50:16 +0530

    Fix indentation and improve error message in circle.py

--- circle.py
+++ circle.py
@@ -1,7 +1,7 @@
 import cv2
 image=cv2.imread(r"c:\Users\Nilesh gupta\Downloads\download (1).jpeg")
 if image is None:
-    print("opps! your image is not working")
+    print("opps! your image  not working")
 else:
     print("image loaded successfully!")
     h,w,c=image.shape
@@ -10,4 +10,5 @@ else:
 
     cv2.imshow("circle showing",image)
     cv2.waitKey(0)
+
     cv2.destroyAllWindows
